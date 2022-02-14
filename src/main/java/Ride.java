e6d83a67114937526658e7ab40b11cb9c6bca064
pankaj kore <korep95@gmail.com>
2022-02-14 19:56:52 +0530

    [pankaj]added feature of calculate fare of multiple rides

---
+++ src/main/java/Ride.java
@@ -0,0 +1,15 @@
+public class Ride {
+    public final double distance;
+    public final int time;
+
+    /**
+     * parameterized constructor declaration
+     * @param distance
+     * @param time
+     */
+    public Ride(double distance, int time) {
+        this.distance=distance;
+        this.time=time;
+
+    }
+}
