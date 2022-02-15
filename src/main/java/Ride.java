715513c9e5e31c98ce44ace84f37991adf71e201
pankaj kore <korep95@gmail.com>
2022-02-15 14:08:13 +0530

    [pankaj]added feature of calculating fare of premium/normal fare

--- src/main/java/Ride.java
+++ src/main/java/Ride.java
@@ -7,7 +7,7 @@ public class Ride {
      * @param distance
      * @param time
      */
-    public Ride(double distance, int time) {
+    public Ride(double distance, int time,PremiumRide premium) {
         this.distance=distance;
         this.time=time;
     }
